4e51e2dbb41c0a29bc7dd355a726897f04dbe870
Oscar <oscdoraba@alum.us.es>
2021-01-14 23:51:07 +0100

    CREA CLASE SUGERENCIA
    Cambios:
     - Introducida clase Sugerencia en booth

--- decide/booth/models.py
+++ decide/booth/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Suggestion(models.Model):
+    suggestion = models.TextField()
+
+def __str__(self):
+        return self.suggestion

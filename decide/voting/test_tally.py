59a70a125121ae5be06a336562da80f4a926be51
Rafa <rafliefue@alum.us.es>
2021-01-18 01:13:56 +0100

    ARREGLO DE ALGUNOS TEST PARA TRAVIS v10
    Se han cambiado algunos test para que travis pueda correrlos sin
    errores.
    
    Cambios:
    - Añadido cambio en test.py de voting.
    
    Tareas asociadas: Tareas 2 y 5
    Proyecto con tareas en
    https://github.com/ivan-design-testing/decide-single-verde-votaciones/projects/1

--- decide/voting/test_tally.py
+++ decide/voting/test_tally.py
@@ -10,7 +10,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
-class TestTallyExport():
+class TestTallyExport(StaticLiveServerTestCase):
 
     def setup_method(self, method):
         self.driver = webdriver.Chrome()

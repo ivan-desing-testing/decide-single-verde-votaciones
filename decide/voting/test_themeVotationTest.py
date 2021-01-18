1c7a57fadbff067582a44faf44b1a13ab9bd9fe9
Iván Cárdenas <ivacarmen@alum.us.es>
2021-01-18 02:41:52 +0100

    PRUEBA TEST UI 2

--- decide/voting/test_themeVotationTest.py
+++ decide/voting/test_themeVotationTest.py
@@ -1,16 +1,14 @@
-import pytest
-import time
-import json
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from django.test import TestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
 from base.tests import BaseTestCase
+
 
 class TestThemeVotationTest(StaticLiveServerTestCase):
      # def setup_method(self, method):
